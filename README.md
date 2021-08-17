8b4140cc292ee18e7696f869a3b0f81f3f16f3a5
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-16 18:19:03 -0700

    clean up ReadMe and app.js

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ need to set variables for the quiz button
 and space for the results to display using getElementById
 add an event listener to the button
     console log, or set an alert to test it's working
-within the event listener we declare the name of the input, and ask our question using the prompt function
-we need to track the results in a variable, and increase the count if the input is a correct answer
+// import functions and grab DOM elements
+//add an event listener to our quiz button to kick off the quiz upon Click
+  // track the local state by declaring a variable and setting it to zero 
+  // ask for first and last name, then ask if they want to continue the quiz
+    // if they input No (or anything that doesn't start with a `y') the quiz will stop
+    //if the user inputs a string beginning with a 'y' the quiz will begin  
+    // if the countsAsYes formula validates the input as true (starting with a Y), increase the value of correctResults by one
 need to alert the user the quiz is over - and to check the page for displayed results
 if the results are 100% we will display ~pretty message, and a 'try again' if the user didn't get all answers correct
 
--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-// import functions and grab DOM elements
+
 import { countsAsYes } from './utils.js';
 
 const quizButton = document.getElementById('quiz-button');
 const results = document.getElementById('results');
 
-//add an event listener to our quiz button to kick off the quiz upon Click
 
 quizButton.addEventListener('click', () => {
-  // track the local state by declaring a variable and setting it to zero 
     let correctResults = 0;
-  // ask for first and last name, then ask if they want to continue the quiz
+
     const firstName = prompt (`Well, howdy, hobgoblin! What's yer first name?`);
     if (!firstName) return;
+
     const lastName = prompt (`So nice to meet ya, ${firstName}. What about your last name?`);
     if (!lastName) return;
+
     const startQuiz = confirm (`Got it, ${firstName} ${lastName}. Are you ready to test your Hobgoblin knowledge?`);
-    // if they input No (or anything that doesn't start with a `y') the quiz will stop
     if (!startQuiz) return;
-    //if the user inputs a string beginning with a 'y' the quiz will begin  
-    // if the countsAsYes formula validates the input as true (starting with a Y), increase the value of correctResults by one
+
     const input1 = prompt(`Can the earliest instance of the word Hobgoblin can be traced to roughly 1530?`);
     if (countsAsYes(input1)) correctResults++;
     console.log(correctResults);
@@ -43,5 +41,5 @@ quizButton.addEventListener('click', () => {
         results.style.fontStyle = 'cursive';
         results.style.color = 'goldenrod';
         results.style.backgroundColor = 'rgb(67, 92, 97)';}
-    else (results.textContent = `Solid effort ${firstName}, you got ${correctResults} out of 4 correct. Try again!`);
+    else (results.textContent = `Solid effort ${firstName}, you got ${correctResults} out of 4 correct :( Try again!`);
 });
