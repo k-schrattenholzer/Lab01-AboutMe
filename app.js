a2f926f2111c887d83e5a816829d6263d2edfc07
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-16 17:21:36 -0700

    add styling to perfect results

--- app.js
+++ app.js
@@ -11,23 +11,40 @@ quizButton.addEventListener('click', () => {
     let correctResults = 0;
   // ask for first and last name, then ask if they want to continue the quiz
     const firstName = prompt (`Well, howdy, hobgoblin! What's yer first name?`);
+    if (!firstName) return;
     const lastName = prompt (`So nice to meet ya, ${firstName}. What about your last name?`);
-    const startQuiz = prompt (`Got it, ${firstName} ${lastName}. Are you ready to test your Hobgoblin knowledge?`);
+    if (!lastName) return;
+    const startQuiz = confirm (`Got it, ${firstName} ${lastName}. Are you ready to test your Hobgoblin knowledge?`);
     // if they input No (or anything that doesn't start with a `y') the quiz will stop
     if (!startQuiz) return;
-    //if the user inputs a string beginning with a 'y' the quiz will begin
-    const input1 = prompt(`(Yes or No) The earliest instance of the word Hobgoblin can be traced to roughly 1530.`);
+    //if the user inputs a string beginning with a 'y' the quiz will begin  
     // if the countsAsYes formula validates the input as true (starting with a Y), increase the value of correctResults by one
+    const input1 = prompt(`Can the earliest instance of the word Hobgoblin can be traced to roughly 1530?`);
     if (countsAsYes(input1)) correctResults++;
-    const input2 = prompt(`(Yes or No) A close relative of the hobgoblin is the Cookie.`);
-    if (countsAsYes(input2)) correctResults++;
-    const input3 = prompt(`(Yes or No) Hobgoblins tout an especially patient and relaxed demeanor.`);
-    if (countsAsYes(input3)) correctResults++;
-    const input4 = prompt(`(Yes or No) Attempts to give them clothing will often banish (or free) them forever.`);
-    if (countsAsYes(input4)) correctResults++;
-    alert (`Wowza - nice job testing your chops ${firstName}! Let's see how you fared...`);
-    results.textContent = `Solid effort ${firstName}, you got ${correctResults} out of 4 correct.`;
+    console.log(correctResults);
 
+    const input2 = prompt(`Is a close relative of the hobgoblin is a Cookie?`);
+    if (!countsAsYes(input2)) correctResults++;
+    console.log(correctResults);
+
+    const input3 = prompt(`Do Hobgoblins typically tout a patient and relaxed demeanor?`);
+    if (!countsAsYes(input3)) correctResults++;
+    console.log(correctResults);
+
+    const input4 = prompt(`If you give one some clothing, will this banish them forever?`);
+
+    if (countsAsYes(input4)) correctResults++;
+    console.log(correctResults);
+
+    alert (`Goodness gracious - ${firstName}! Let's see how you fared...`);
+
+    if (correctResults > 3) {
+        results.textContent = `INCREDIBLE!! You truly are a Hobgoblin expert!`;
+        results.style.fontStyle = 'cursive';
+        results.style.color = 'goldenrod';
+        results.style.backgroundColor = 'black';
+        results.style.opacity = '50%';}
+    else (results.textContent = `Solid effort ${firstName}, you got ${correctResults} out of 4 correct. Try again!`);
 });
 
 // set event listeners 
